cd0ec704c66bb719ce4803e9893254a8877620f6
Diego Romero <diego.romero@unleashtechnologies.net>
2023-03-19 13:05:25 -0600

    Responsive Header

--- src/pages/Home/components/HomeHero/HomeHero.styles.js
+++ src/pages/Home/components/HomeHero/HomeHero.styles.js
@@ -39,7 +39,7 @@ export const useHomeHeroStyles = createStyles((theme) => ({
     },
 
     desktopNavbarCTA: {
-        filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
+        filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.26))",
         borderRadius: theme.radius.sm
     },
 
