98276d7aa0b23a0cf7bab67deb5b8b715ad5aa22
Diego Romero <diego.romero@unleashtechnologies.net>
2023-03-19 18:56:20 -0600

    minecraftLand3d.webp

--- src/pages/Home/components/BannerMinecraft/BannerMinecraft.jsx
+++ src/pages/Home/components/BannerMinecraft/BannerMinecraft.jsx
@@ -1,32 +1,32 @@
-import { Button, Stack, Group, Card, Overlay, Title, BackgroundImage, Image, Box, Text } from '@mantine/core';
-import minecraft3dImage from './static/minecraftLand3d.svg'
+import {Button, Card, Group, Image, Stack, Text} from '@mantine/core';
+import minecraft3dImage from './static/minecraftLand3d.webp'
 import React from 'react';
-import { useBannerMinecraftStyles } from './BannerMinecraft.styles';
+import {useBannerMinecraftStyles} from './BannerMinecraft.styles';
 
 
+function BannerMinecraft() {
 
-function BannerMinecraft(props){
-
-   const {classes} = useBannerMinecraftStyles();
-    return( 
-        <Group m={'md'} p={0} sx={{justifyContent:'space-around',}}>
-                <Card
+    const {classes} = useBannerMinecraftStyles();
+    return (
+        <Group m={'md'} p={0} sx={{justifyContent: 'space-around',}}>
+            <Card
                 shadow="sm"
-                sx={{backgroundColor:'#0CA678', flexGrow:1}}
+                sx={{backgroundColor: '#0CA678', flexGrow: 1}}
                 h={184}
                 w='42%'
                 p={'sm'}
                 m={0}
-                >
-                    <Stack>
-                        <Text className={classes.textStyle} mt={'xs'} color="white" align='center' >
-                            Cultiva las Habilidades STEM con Minecraft
-                        </Text>
-                        <Button className={classes.bannerButton} color='red.6' >¡Inscribite Ahora!</Button>
-                    </Stack>
-                </Card>
-            <Image maw={'45%'} radius="md" src={minecraft3dImage} alt="Random image" />
+            >
+                <Stack>
+                    <Text className={classes.textStyle} mt={'xs'} color="white" align='center'>
+                        Cultiva las Habilidades STEM con Minecraft
+                    </Text>
+                    <Button className={classes.bannerButton} color='red.6'>¡Inscribite Ahora!</Button>
+                </Stack>
+            </Card>
+            <Image maw={'45%'} radius="md" src={minecraft3dImage} alt="Random image"/>
         </Group>
     )
 }
+
 export default BannerMinecraft;
