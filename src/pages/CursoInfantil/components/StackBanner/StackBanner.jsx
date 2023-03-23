63542874278b3df600e413cd4c1746e27f1adadd
Diego Romero <diego.romero@unleashtechnologies.net>
2023-03-23 16:06:33 -0600

    Stack Banner Styles

--- src/pages/CursoInfantil/components/StackBanner/StackBanner.jsx
+++ src/pages/CursoInfantil/components/StackBanner/StackBanner.jsx
@@ -1,13 +1,15 @@
 import {Image, Stack, Text, Title} from '@mantine/core';
 import React from 'react';
+import {useStackBannerStyles} from "./StackBanner.styles";
 
 
-function StackBanner(props) {
-    const {mainImage, title} = props;
+function StackBanner({mainImage, title}) {
+    const {classes} = useStackBannerStyles()
     return (
         <Stack>
-            <Title order={4} weight={500}>{title}</Title>
-            <Text fz={14}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin et enim incidunt
+            <Title order={4} weight={500} className={classes.title}>{title}</Title>
+            <Text className={classes.content}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                sollicitudin et enim incidunt
                 laoreet. Nunc auc tor eu ante in blandit. In interdum vel elit eu sollicitudin Ut pretium blandit
                 tincidunt. Vestibulum congue est felis, at tristique tellus rhoncus eu. </Text>
             <Image width={'100%'} mx="auto" radius="md" src={mainImage}/>
--- src/pages/CursoInfantil/components/StackBanner/StackBanner.styles.js
+++ src/pages/CursoInfantil/components/StackBanner/StackBanner.styles.js
@@ -2,5 +2,20 @@ import {createStyles} from '@mantine/core';
 
 export const useStackBannerStyles = createStyles((theme) => ({
 
-    
+    title: {
+        fontSize: 20,
+
+        [theme.fn.largerThan("sm")]: {
+            fontSize: 24
+        }
+    },
+
+    content: {
+        fontSize: 14,
+
+        [theme.fn.largerThan("sm")]: {
+            fontSize: 16
+        }
+    },
+
 }))
