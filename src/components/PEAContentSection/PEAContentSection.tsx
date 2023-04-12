1f3f75c6d3633bb339727559843e9b00b16ea412
FernandoNarvaez1904 <47584735+FernandoNarvaez1904@users.noreply.github.com>
2023-04-12 10:16:11 -0600

    spCNG

--- src/components/PEAContentSection/PEAContentSection.tsx
+++ src/components/PEAContentSection/PEAContentSection.tsx
@@ -1,54 +1,46 @@
-import {
-    Box,
-    DefaultMantineColor,
-    Text,
-    List
-  } from "@mantine/core";
+import { Box, DefaultMantineColor, List, Stack, Text } from "@mantine/core";
 
 import { ICourseDescriptionCardElement } from "../CourseDescriptionCard/CourseDescriptionCard";
-
-
-
-
-
-export interface IPEAContentSectiosProps{
-    textContent:string;
-    listContent?:string[]
-    courseInfo:{
-        cardImage:string,
-        color:DefaultMantineColor,
-        title:string,
-        elements:ICourseDescriptionCardElement[],
-    }
-}
-
 import CourseDescriptionCard from "../CourseDescriptionCard";
 import usePEAContentSectionStyles from "./PEAContentSection.styles";
 
-
-function PEAContentSection({
-    textContent,
-    listContent,
-    courseInfo
-}:IPEAContentSectiosProps){
-    const {classes} = usePEAContentSectionStyles();
-    const listContentElements = listContent?.map((element) => (
-        <List.Item>{element}</List.Item> 
-        )
-    )
-    return(
-        <Box className={classes.grid}>
-            <Box>
-                <Text className={classes.textContentStyle} mb={20}>{textContent}</Text>
-                <List className={classes.listContentStyle}>
-                    {listContentElements}
-                </List>
-
-
-            </Box>
-            <CourseDescriptionCard mainImage={courseInfo.cardImage} color={courseInfo.color} title={courseInfo.title} elements={courseInfo.elements} />
-        </Box>
-    )
+export interface IPEAContentSectiosProps {
+  textContent: string;
+  listContent?: string[];
+  courseInfo: {
+    cardImage: string;
+    color: DefaultMantineColor;
+    title: string;
+    elements: ICourseDescriptionCardElement[];
+  };
 }
 
-export default PEAContentSection
+function PEAContentSection({
+  textContent,
+  listContent,
+
+  courseInfo,
+}: IPEAContentSectiosProps) {
+  const { classes } = usePEAContentSectionStyles();
+  const listContentElements = listContent?.map((element) => (
+    <List.Item>{element}</List.Item>
+  ));
+  return (
+    <Stack spacing="xl" className={classes.grid}>
+      <Box>
+        <Text className={classes.textContentStyle} mb={20}>
+          {textContent}
+        </Text>
+        <List className={classes.listContentStyle}>{listContentElements}</List>
+      </Box>
+      <CourseDescriptionCard
+        mainImage={courseInfo.cardImage}
+        color={courseInfo.color}
+        title={courseInfo.title}
+        elements={courseInfo.elements}
+      />
+    </Stack>
+  );
+}
+
+export default PEAContentSection;
--- src/templates/PEACourseTemplate/PEACourseTemplate.tsx
+++ src/templates/PEACourseTemplate/PEACourseTemplate.tsx
@@ -1,47 +1,39 @@
 // React
-import {
-    DefaultMantineColor,
-    Stack,
-    useMantineTheme,
-  } from "@mantine/core";
+import { DefaultMantineColor, Stack, useMantineTheme } from "@mantine/core";
 
-  import { useMediaQuery } from "@mantine/hooks";
+import { useMediaQuery } from "@mantine/hooks";
 
-  // Components
-  import ColoredHeader from "../../components/ColoredHeader";
-  import PEAContentSection from "../../components/PEAContentSection";
-  import PEAHero from "../../components/PEAHero/PEAHero";
+// Components
+import ColoredHeader from "../../components/ColoredHeader";
+import PEAContentSection from "../../components/PEAContentSection";
+import PEAHero, { IPEAHeroProps } from "../../components/PEAHero/PEAHero";
 
-  //Interfaces
+// Interfaces
+import { IPEAContentSectiosProps } from "../../components/PEAContentSection/PEAContentSection";
 
-  import { IPEAContentSectiosProps } from "../../components/PEAContentSection/PEAContentSection";
-  import { IPEAHeroProps } from "../../components/PEAHero/PEAHero";
-
-
-export interface IPEACourseTemplateProps{
-    accentColor:DefaultMantineColor;
-    PEAContentInfo:IPEAContentSectiosProps;
-    PEAHeroInfo:IPEAHeroProps;
+export interface IPEACourseTemplateProps {
+  accentColor: DefaultMantineColor;
+  PEAContentInfo: IPEAContentSectiosProps;
+  PEAHeroInfo: IPEAHeroProps;
 }
 
 function PEACourseTemplate({
-    accentColor,
-    PEAContentInfo,
-    PEAHeroInfo
-}:IPEACourseTemplateProps){
-    const theme = useMantineTheme();
-    const isLargeScreen = useMediaQuery(`(min-width: ${theme.breakpoints.sm} )`);
-    const xPadding = isLargeScreen ? 128 : "xs";
-return(
-    <Stack spacing={"xl"}>
-        <ColoredHeader color={accentColor}/>
-        <PEAHero {...PEAHeroInfo}/>
-
-        <Stack px={xPadding} spacing="sm" pb={30}>
-            <PEAContentSection {... PEAContentInfo}/>
-        </Stack>
+  accentColor,
+  PEAContentInfo,
+  PEAHeroInfo,
+}: IPEACourseTemplateProps) {
+  const theme = useMantineTheme();
+  const isLargeScreen = useMediaQuery(`(min-width: ${theme.breakpoints.sm} )`);
+  const xPadding = isLargeScreen ? 128 : "xs";
+  return (
+    <Stack spacing="xl">
+      <ColoredHeader color={accentColor} />
+      <PEAHero {...PEAHeroInfo} />
+      <Stack px={xPadding} spacing="xl" pb={30}>
+        <PEAContentSection {...PEAContentInfo} />
+      </Stack>
     </Stack>
-)
+  );
 }
 
 export default PEACourseTemplate;
